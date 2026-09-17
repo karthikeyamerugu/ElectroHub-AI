dadb02c91faf55f9bfbd67ddc4d3e51753d40c14
Karthikeya merugu <karthikmerugu2009@gmail.com>
2026-09-17 09:25:39 +0530

    Update service worker to use new cache version

--- service-worker.js
+++ service-worker.js
@@ -1,43 +1,193 @@
-const CACHE_NAME = "electrohub-ai-v1";
+"use strict";
 
-const FILES_TO_CACHE = [
+
+const CACHE_NAME =
+    "electrohub-ai-v2";
+
+
+const APP_FILES = [
+
     "./",
+
     "./index.html",
+
     "./style.css",
+
     "./app.js",
-    "./manifest.json"
+
+    "./manifest.json",
+
+    "./icon-192.png",
+
+    "./icon-512.png"
+
 ];
 
-self.addEventListener("install", function(event) {
-    event.waitUntil(
-        caches.open(CACHE_NAME).then(function(cache) {
-            return cache.addAll(FILES_TO_CACHE);
-        })
-    );
 
-    self.skipWaiting();
-});
+self.addEventListener(
+    "install",
+    function(event) {
 
-self.addEventListener("activate", function(event) {
-    event.waitUntil(
-        caches.keys().then(function(cacheNames) {
-            return Promise.all(
-                cacheNames.map(function(cacheName) {
-                    if (cacheName !== CACHE_NAME) {
-                        return caches.delete(cacheName);
-                    }
+        event.waitUntil(
+
+            caches
+                .open(CACHE_NAME)
+                .then(function(cache) {
+
+                    return cache.addAll(
+                        APP_FILES
+                    );
+
                 })
+
+        );
+
+
+        self.skipWaiting();
+
+    }
+);
+
+
+self.addEventListener(
+    "activate",
+    function(event) {
+
+        event.waitUntil(
+
+            caches
+                .keys()
+                .then(function(cacheNames) {
+
+                    return Promise.all(
+
+                        cacheNames.map(
+                            function(cacheName) {
+
+                                if (
+                                    cacheName !==
+                                    CACHE_NAME
+                                ) {
+
+                                    return caches.delete(
+                                        cacheName
+                                    );
+
+                                }
+
+                            }
+                        )
+
+                    );
+
+                })
+
+        );
+
+
+        self.clients.claim();
+
+    }
+);
+
+
+self.addEventListener(
+    "fetch",
+    function(event) {
+
+        const request =
+            event.request;
+
+
+        /*
+         * The AI request uses POST.
+         * We don't cache API requests.
+         */
+
+        if (
+            request.method !== "GET"
+        ) {
+
+            return;
+
+        }
+
+
+        /*
+         * Only cache files belonging
+         * to this website.
+         */
+
+        const url =
+            new URL(
+                request.url
             );
-        })
-    );
 
-    self.clients.claim();
-});
 
-self.addEventListener("fetch", function(event) {
-    event.respondWith(
-        caches.match(event.request).then(function(cachedResponse) {
-            return cachedResponse || fetch(event.request);
-        })
-    );
-});
+        if (
+            url.origin !==
+            self.location.origin
+        ) {
+
+            return;
+
+        }
+
+
+        event.respondWith(
+
+            caches
+                .match(request)
+                .then(function(cachedResponse) {
+
+                    if (cachedResponse) {
+
+                        return cachedResponse;
+
+                    }
+
+
+                    return fetch(request)
+                        .then(function(response) {
+
+                            if (
+                                !response ||
+                                response.status !== 200 ||
+                                response.type !== "basic"
+                            ) {
+
+                                return response;
+
+                            }
+
+
+                            const responseClone =
+                                response.clone();
+
+
+                            caches
+                                .open(
+                                    CACHE_NAME
+                                )
+                                .then(
+                                    function(cache) {
+
+                                        cache.put(
+                                            request,
+                                            responseClone
+                                        );
+
+                                    }
+                                );
+
+
+                            return response;
+
+                        });
+
+                })
+
+        );
+
+    }
+);
